f888956c58f31164275ebf1912ec6af52a4b82d4
Maverick <Mave3624@gmail.com>
2024-12-18 10:20:22 +0000

    added boreder color

--- script/java.js
+++ script/java.js
@@ -1,2 +1,11 @@
+const image =document.querySelectorAll('.imgHolder')
 
+function transform(color) {
+  color = '#'+Math.floor(Math.random() * 1000000)
+  for(a= 0; a<image.length; a++) {
+    image[a].style.border = '2px solid ' + color
+  }
+  setTimeout(() => transform(), 1000)
+}
 
+transform()
